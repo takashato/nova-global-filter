6250a48b2c4f4e2338b75ca9c4b7c43d60221d82
Son Banh <takashato@gmail.com>
2022-12-08 10:43:54 +0700

    allow html in table metric

--- resources/js/card.js
+++ resources/js/card.js
@@ -4,6 +4,7 @@ Nova.booting((Vue, router, store) => {
   Vue.component("TrendMetric", require("./components/TrendMetric.vue").default);
   Vue.component("ValueMetric", require("./components/ValueMetric.vue").default);
   Vue.component("TableMetric", require("./components/TableMetric.vue").default);
+  Vue.component("MetricTableRow", require("./components/MetricTableRow.vue").default);
   Vue.component("NovaGlobalFilter", require("./components/GlobalFilter.vue").default);
   // Vue.component('LoadingCard', require('./components/LoadingCard.vue').default)
   window.Vue.use = () => {
